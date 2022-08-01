95845e63c5c5826ef49fd7fdd30dee0de0624636
mihika123945 <80121984+mihika123945@users.noreply.github.com>
2022-08-02 02:34:21 +0530

    dialogue

--- Bank Game/Assets/NewDialogueManager.cs
+++ Bank Game/Assets/NewDialogueManager.cs
@@ -8,31 +8,41 @@ public class NewDialogueManager : MonoBehaviour
     [SerializeField] private float typingSpeed = 0.05f;
 
     [SerializeField] private bool PlayerSpeakingFirst;
-
+    [Header("Dialogue TMP text")]
     [SerializeField] private TextMeshProUGUI playerDialogueText;
     [SerializeField] private TextMeshProUGUI nPCDialogueText;
-
+    [Header("Animation Controllers")]
+    [SerializeField] private Animator playerSpeechBubbleAnimator;
+    [SerializeField] private Animator nPCSpeechBubbleAnimator;
+    [Header("Dialogue Sentences")]
     [SerializeField] private string[] playerDialogueSentences;
     [SerializeField] private string[] nPCDialogueSentences;
 
+    [Header("Dialogue Buttons")]
     [SerializeField] private GameObject playerContinueButton;
     [SerializeField] private GameObject nPCContinueButton;
+    private bool dialogueStarted;
 
     private int playerIndex;
     private int nPCIndex;
+    private float speechBubbleAnimationDelay = 0.6f;
 
 
     private void Start(){
-        StartDialogue();
+        StartCoroutine(StartDialogue());
      }
 
-    public void StartDialogue()
+    public IEnumerator StartDialogue()
     {
         if (PlayerSpeakingFirst){
+            playerSpeechBubbleAnimator.SetTrigger("Open");
+            yield return  new WaitForSeconds(speechBubbleAnimationDelay);
             StartCoroutine(TypePlayerDialogue());
         }
         else
         {
+            nPCSpeechBubbleAnimator.SetTrigger("Open");
+            yield return  new WaitForSeconds(speechBubbleAnimationDelay);
             StartCoroutine(TypeNPCDialogue());
         }
     }
@@ -57,5 +67,56 @@ public class NewDialogueManager : MonoBehaviour
         }
 
         nPCContinueButton.SetActive(true);
+    }
+    private IEnumerator ContinuePlayerDialogue()
+    {
+        nPCDialogueText.text = string.Empty;
+        nPCSpeechBubbleAnimator.SetTrigger("Close");
+        yield return  new WaitForSeconds(speechBubbleAnimationDelay);
+        
+        playerDialogueText.text = string.Empty;
+        playerSpeechBubbleAnimator.SetTrigger("Open");
+        yield return  new WaitForSeconds(speechBubbleAnimationDelay);
+
+        nPCContinueButton.SetActive(false);
+        if(playerIndex < playerDialogueSentences.Length - 1)
+        {
+            if(dialogueStarted)
+            playerIndex++;
+            else 
+               dialogueStarted = true;
+
+            playerDialogueText.text = string.Empty;
+            StartCoroutine(TypePlayerDialogue());
+        }
+    }
+    public IEnumerator ContinueNPCDialogue()
+    {
+        playerDialogueText.text = string.Empty;
+        playerSpeechBubbleAnimator.SetTrigger("Close");
+        yield return  new WaitForSeconds(speechBubbleAnimationDelay);
+
+        nPCDialogueText.text = string.Empty;
+        nPCSpeechBubbleAnimator.SetTrigger("Open");
+        yield return  new WaitForSeconds(speechBubbleAnimationDelay);
+        playerContinueButton.SetActive(false);
+        if(nPCIndex < nPCDialogueSentences.Length - 1)
+        {
+            if(dialogueStarted)
+            nPCIndex++;
+            else 
+               dialogueStarted = true;
+
+            nPCDialogueText.text = string.Empty;
+            StartCoroutine(TypeNPCDialogue());
+        }
+    }
+
+    public void TriggerContinuePlayerDialogue(){
+        StartCoroutine(ContinuePlayerDialogue());
+    }
+
+    public void TriggerContinueNPCDialogue(){
+      StartCoroutine(ContinueNPCDialogue());
     }
 }
