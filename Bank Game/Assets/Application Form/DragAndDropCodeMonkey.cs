3418035fb9543b82f5701d42a5574877ab391937
Jayneet123 <68434045+Jayneet123@users.noreply.github.com>
2022-08-17 01:36:00 +0530

    drag n drop

--- Bank Game/Assets/Application Form/DragAndDropCodeMonkey.cs
+++ Bank Game/Assets/Application Form/DragAndDropCodeMonkey.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class DragAndDropCodeMonkey : MonoBehaviour , IPointerDownHandler , IBeginDragHandler , IEndDragHandler , IDragHandler
 {
     [SerializeField] private Canvas canvas;
     private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
 
     private void Awake(){
         rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
     }
 
     public void OnPointerDown(PointerEventData eventData){
@@ -18,14 +21,22 @@ public class DragAndDropCodeMonkey : MonoBehaviour , IPointerDownHandler , IBegi
 
     public void OnBeginDrag(PointerEventData eventData){
         Debug.Log("OnBeginDrag");
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0.6f;
     }
 
     public void OnDrag(PointerEventData eventData){
         Debug.Log("OnDrag");
-        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        Vector2 position;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle((RectTransform)canvas.transform,
+        eventData.position,canvas.worldCamera,out position);
+        transform.position=canvas.transform.TransformPoint(position);
     }
 
     public void OnEndDrag(PointerEventData eventData){
         Debug.Log("OnEndDrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = false;
     }
+
 }
