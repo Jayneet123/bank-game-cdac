b89ae849327196e055530406319ada63d5ff7f82
Jayneet123 <68434045+Jayneet123@users.noreply.github.com>
2022-08-17 23:11:59 +0530

    form manager added

---
+++ Bank Game/Assets/FormManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class FormManager : MonoBehaviour
+{
+    public GameObject aadharNumber;
+    public GameObject aadharNumberOnCard;
+    private int counter =0;
+    
+    public void onSubmit(){
+        if (aadharNumber.tag == aadharNumberOnCard.tag){
+            counter++;
+            Debug.Log("Counter");
+        }
+    }
+}
