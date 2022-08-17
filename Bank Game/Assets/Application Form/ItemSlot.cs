11343bed5b95019ff2491d6a00a2a327fb42b0ea
Jayneet123 <68434045+Jayneet123@users.noreply.github.com>
2022-08-17 11:37:48 +0530

    drag n drop done

--- Bank Game/Assets/Application Form/ItemSlot.cs
+++ Bank Game/Assets/Application Form/ItemSlot.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class ItemSlot : MonoBehaviour , IDropHandler
 {
+    public Text name;
+    public Text onNameField;
     public void OnDrop(PointerEventData eventData){
         Debug.Log("OnDrop");
-        if (eventData.pointerDrag != null){
-            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
-        }
+        // if (eventData.pointerDrag != null){
+        //     eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        // }
+        onNameField.text = name.text;
+        name.text = " ";
+        
     }
 }
