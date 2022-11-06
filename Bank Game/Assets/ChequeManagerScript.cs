0a657da5a0a9c65c2473cee231436d9a69186e3b
Jayneet123 <68434045+Jayneet123@users.noreply.github.com>
2022-11-07 00:17:25 +0530

    minor changes and fixed bugs

--- Bank Game/Assets/ChequeManagerScript.cs
+++ Bank Game/Assets/ChequeManagerScript.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ChequeManagerScript : MonoBehaviour
 {
     public TextMeshProUGUI score;
     public GameObject try1;
     public GameObject mail;
+    public GameObject success;
+    public GameObject failed;
 
     public TextMeshProUGUI chequeName;
     public TMP_InputField chequeNameInput;
@@ -20,7 +23,7 @@ public class ChequeManagerScript : MonoBehaviour
     public TextMeshProUGUI sign;
     public TMP_InputField signInput;
 
-    private int counter=0;
+    public int counter=0;
 
     private string[] chequeNames = {"John Shah"};
     private string[] dates = {"01/10/2022"};
@@ -44,15 +47,25 @@ public class ChequeManagerScript : MonoBehaviour
         mail.SetActive(true);
         if((chequeNameInput.text==chequeNames[0])&&(dateInput.text==dates[0])&&(rupeeInput.text==rupees[0])&&(textRupeeInput.text==textRupees[0])&&(signInput.text==signs[0])){
             counter++;
-            
         }
-       
+        Debug.Log("Success");
     }
 
     public void onMailClick(){
-        // try1.SetActive(true);
+        Debug.Log(counter);
         if(counter==0){
-            
+            failed.SetActive(true);
         }
+        if(counter==1){
+            success.SetActive(true);
+        }
+    }
+
+    public void onSuccessContinue(){
+        SceneManager.LoadScene("Start");
+    }
+    public void onFailContinue(){
+        FormManager.Score -= 10;
+        SceneManager.LoadScene("Cheque");
     }
 }
