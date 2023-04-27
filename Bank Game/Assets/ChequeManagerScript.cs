bdcad53e1addf14712f398308dc05647317289ec
jayneet <jaynitjain639@gmail.com>
2023-04-27 18:00:34 +0530

    lvl2 done

--- Bank Game/Assets/ChequeManagerScript.cs
+++ Bank Game/Assets/ChequeManagerScript.cs
@@ -36,6 +36,7 @@ public class ChequeManagerScript : MonoBehaviour
     private string[] signs = {"RTomar.","RShah","DDoshi.","MGupta.","AGupta."};
 
     // Cheque slip variables
+    private string[] slipSigns = {"Ramesh Tomar","Ramesh Shah","Dinesh Doshi","Manish Gupta","Akash Gupta"};
     public Canvas inputCanvas;
     public GameObject slip;
     private string[] descriptions = {"Additional Information\n\n\nYou are Ramesh Tomar.\nYou are writing a cheque to John Shah to pay a sum of 50000 rupees on the date 01/10/2022.\nYou are transferring the money from a/c no: IDIB000K023 to a/c no: DDIC000M057.\nYou live in Malad West, Mumbai, Maharshtra.\nPlease fill the reciept to complete the cheque deposit.","Additional Information\n\n\nYou are Ramesh Shah.\nYou are writing a cheque to Jack Das to pay a sum of 30000 rupees on the date 02/02/2022.\nYou are transferring the money from a/c no: IDIB000K023 to a/c no: DDIC000M057.\nYou live in Malad West, Mumbai, Maharshtra.\nPlease fill the reciept to complete the cheque deposit.","Additional Information\n\n\nYou are Dinesh Doshi.\nYou are writing a cheque to Raj Tomar to pay a sum of 28000 rupees on the date 30/04/2022.\nYou are transferring the money from a/c no: IDIB000K023 to a/c no: DDIC000M057.\nYou live in Malad West, Mumbai, Maharshtra.\nPlease fill the reciept to complete the cheque deposit.","Additional Information\n\n\nYou are Manish Gupta.\nYou are writing a cheque to Riya Shah to pay a sum of 80000 rupees on the date 21/08/2022.\nYou are transferring the money from a/c no: IDIB000K023 to a/c no: DDIC000M057.\nYou live in Malad West, Mumbai, Maharshtra.\nPlease fill the reciept to complete the cheque deposit.","Additional Information\n\n\nYou are Akash Gupta.\nYou are writing a cheque to Alice Gupta to pay a sum of 20000 rupees on the date 31/10/2022.\nYou are transferring the money from a/c no: IDIB000K023 to a/c no: DDIC000M057.\nYou live in Malad West, Mumbai, Maharshtra.\nPlease fill the reciept to complete the cheque deposit.",};
@@ -72,22 +73,25 @@ public class ChequeManagerScript : MonoBehaviour
         slip.SetActive(true);
         if((chequeNameInput.text==chequeNames[j])&&(dateInput.text==dates[j])&&(rupeeInput.text==rupees[j])&&(textRupeeInput.text==textRupees[j])&&(signInput.text==signs[j])){
             counter++;
+            Debug.Log(counter);
+            Debug.Log("Success Cheque");
         }
-        Debug.Log("Success");
+        
         inputCanvas.enabled = false;
         robotCheque.SetActive(false);
     }
     public void onSlipSubmit(){
-        if((branch_input.text=="Malad West")&&(date_input.text==dates[j])&&(totalrs_input.text==rupees[j])&&(amount_input.text==textRupees[j])&&(credit_input.text==chequeNames[j])&&(ac_input.text=="IDIB000K023")){
+        if((branch_input.text=="Malad West")&&(date_input.text==dates[j])&&(totalrs_input.text==rupees[j])&&(amount_input.text==textRupees[j])&&(credit_input.text==chequeNames[j])&&(ac_input.text=="IDIB000K023")&&(deposited_input.text==slipSigns[j])){
             slipcounter++;
             Debug.Log(slipcounter);
+            Debug.Log("Success Slip");
         }
         slip.SetActive(false);
         mail.SetActive(true);
     }
 
     public void onMailClick(){
-        Debug.Log(counter);
+        // Debug.Log(counter);
         if(counter==0|slipcounter==0){
             failed.SetActive(true);
         }
