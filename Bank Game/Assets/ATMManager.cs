e3d807d804999cd80628e91bba3289d1bd256198
jayneet <jaynitjain639@gmail.com>
2023-05-11 22:41:45 +0530

    ...

--- Bank Game/Assets/ATMManager.cs
+++ Bank Game/Assets/ATMManager.cs
@@ -9,10 +9,12 @@ public class ATMManager : MonoBehaviour
     public GameObject background;
     public GameObject atm;    
     public GameObject creditCard;    
-    public GameObject atmModel;        
+    public GameObject atmModel;     
+    public GameObject note;   
 
     //ATM Working
     public Animator cardAnimator;
+    public Animator noteAnimator;
     public GameObject insertCard;
     public GameObject insertPin;
     public GameObject cashDeposit;
@@ -21,12 +23,16 @@ public class ATMManager : MonoBehaviour
     public GameObject processing;
     public GameObject collectCash;
     public TMP_InputField inputPin;
-    public TextMeshProUGUI Button;
+    public TextMeshProUGUI enterButton;
+    public TextMeshProUGUI rightButton;
+    public TextMeshProUGUI leftButton;
+    public int counter=0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        atmModel.SetActive(true);
+        background.SetActive(true);        
     }
 
     public void onAtmEnter(){
@@ -49,13 +55,36 @@ public class ATMManager : MonoBehaviour
     }
 
     public IEnumerator EnterClick(){
-        insertPin.SetActive(false);
-        processing.SetActive(true);
-        yield return new WaitForSeconds(3);
-        accountType.SetActive(true);
+            if(counter==0){
+                insertPin.SetActive(false);
+                processing.SetActive(true);
+                yield return new WaitForSeconds(3);
+                accountType.SetActive(true);
+                counter++;
+            }
+            else if(counter==1){
+                withdraw.SetActive(false);
+                processing.SetActive(true);
+                yield return new WaitForSeconds(2);
+                collectCash.SetActive(true);
+                //Animation
+                note.SetActive(true);
+                noteAnimator.SetTrigger("isWithdrawn");
+            }
     }
 
     public void onEnterClick(){
         StartCoroutine(EnterClick());
+    }
+
+    public IEnumerator onSideButtonClick(){
+        accountType.SetActive(false);
+        processing.SetActive(true);
+        yield return new WaitForSeconds(3);
+        withdraw.SetActive(true);
+    }
+
+    public void onSideClick(){
+        StartCoroutine(onSideButtonClick());
     }
 }
--- Bank Game/Assets/ButtonController.cs
+++ Bank Game/Assets/ButtonController.cs
@@ -7,13 +7,20 @@ using TMPro;
 public class ButtonController : MonoBehaviour
 {
     public TextMeshProUGUI buttonText;
-    public TMP_InputField input;
+    public TMP_InputField inputPin;
+    public bool pinfinished=false;
+    public TMP_InputField inputAmt;
 
     // Update is called once per frame
     public void ValueWhenButtonClicked()
     {
-        if(input.text.Length<4)
-        input.text += buttonText.text;
+        if(inputPin.text.Length<4){
+            inputPin.text += buttonText.text;
+            pinfinished = true;
+        }    
+        else if(inputAmt.text.Length<4){
+            inputAmt.text += buttonText.text;
+        }
+            
     }
-
 }
